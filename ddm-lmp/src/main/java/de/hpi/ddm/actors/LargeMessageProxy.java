f4e8c903ade83be0e594ed6f9e02ab3c0284f07c
Maximilian Schall <Maximilian.Schall@mail.de>
2019-11-07 17:52:11 +0100

    Intermediate result

--- ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -1,17 +1,23 @@
 package de.hpi.ddm.actors;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
 
-import akka.actor.AbstractLoggingActor;
-import akka.actor.ActorRef;
-import akka.actor.ActorSelection;
-import akka.actor.Props;
+import akka.NotUsed;
+import akka.actor.*;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import akka.util.Timeout;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import scala.concurrent.Await;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
 
 public class LargeMessageProxy extends AbstractLoggingActor {
 
@@ -43,11 +49,26 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		private ActorRef sender;
 		private ActorRef receiver;
 	}
+
+	@Data @NoArgsConstructor @AllArgsConstructor
+	static class AskReference implements Serializable{
+		private static final long serialVersionUID = 4057807743851319842L;
+
+		private ActorRef sender;
+	}
+
+	@Data @NoArgsConstructor @AllArgsConstructor
+	static class AnswerReference implements Serializable{
+		private static final long serialVersionUID = 4057802342472319842L;
+
+		private ActorRef sender;
+	}
 	
 	/////////////////
 	// Actor State //
 	/////////////////
-	
+
+	LargeMessage<?> storage = null;
 	/////////////////////
 	// Actor Lifecycle //
 	/////////////////////
@@ -55,12 +76,15 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	////////////////////
 	// Actor Behavior //
 	////////////////////
-	
+
 	@Override
 	public Receive createReceive() {
 		return receiveBuilder()
 				.match(LargeMessage.class, this::handle)
 				.match(BytesMessage.class, this::handle)
+				.match(AskReference.class, this::handle)
+				.match(AnswerReference.class, this::handle)
+				.match(ActorIdentity.class, this::handle)
 				.matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
 				.build();
 	}
@@ -68,20 +92,11 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	private void handle(LargeMessage<?> message) {
 		ActorRef receiver = message.getReceiver();
 		ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
-		File tempFile = null;
-		Kryo kryo = new Kryo();
-		try{
-			File tempDir = new File(System.getProperty("java.io.tmpdir"));
-			tempFile = File.createTempFile("largeMessage", ".tmp", tempDir);
-			FileOutputStream file = new FileOutputStream(tempFile);
-			Output output = new Output(file);
-			kryo.writeClassAndObject(output, message.getMessage());
-			output.close();
-			file.close();
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		if(this.storage != null)
+			System.out.println("\n\n\n\n\n\n\n");
+		this.storage = message;
+		AskReference askReference = new AskReference(this.self());
+		receiverProxy.tell(new Identify(new byte[0]) , this.self());
 
 		// This will definitely fail in a distributed setting if the serialized message is large!
 		// Solution options:
@@ -90,7 +105,46 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		// 2. Serialize the object and send its bytes via Akka streaming.
 		// 3. Send the object via Akka's http client-server component.
 		// 4. Other ideas ...
-		receiverProxy.tell(new BytesMessage<>(tempFile.getAbsolutePath(), this.sender(), message.getReceiver()), this.self());
+		//receiverProxy.tell(new BytesMessage<>(tempFile.getAbsolutePath(), this.sender(), message.getReceiver()), this.self());
+	}
+
+	private void handle(ActorIdentity message){
+		ActorRef actorRef = message.getActorRef().get();
+		File tempFile = null;
+		Kryo kryo = new Kryo();
+		ByteArrayOutputStream outputStream = null;
+		ActorRef shardActor = null;
+
+
+		try{
+			outputStream = new ByteArrayOutputStream();
+			Output output = new Output(outputStream);
+			kryo.writeClassAndObject(output, storage.getMessage());
+			output.close();
+			outputStream.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+
+		Source<byte[], NotUsed> source = Source.from(Arrays.asList(outputStream.toByteArray()));
+
+		Sink<byte[], NotUsed> sink =
+				Sink.<byte[]>actorRefWithAck(
+						actorRef,
+						new StreamInitialized(),
+						Ack.INSTANCE,
+						new StreamCompleted(),
+						ex -> new StreamFailure(ex));
+		source.to(sink);
+	}
+
+	private void handle(AskReference message) {
+		message.getSender().tell(new AnswerReference(this.sender()), this.self());
+	}
+
+	private void handle(AnswerReference message) {
+
 	}
 
 	private void handle(BytesMessage<?> message) {
@@ -106,4 +160,27 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		}catch(Exception e){}
 		message.getReceiver().tell(object, message.getSender());
 	}
+
+	enum Ack {
+		INSTANCE;
+	}
+
+	static class StreamInitialized {}
+
+	static class StreamCompleted {}
+
+	static class StreamFailure {
+		private final Throwable cause;
+
+		public StreamFailure(Throwable cause) {
+			this.cause = cause;
+		}
+
+		public Throwable getCause() {
+			return cause;
+		}
+	}
+
 }
+
+
