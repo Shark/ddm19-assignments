8722e5d0306076b26adf0dcb685d2c18457991c5
Maximilian Schall <Maximilian.Schall@mail.de>
2019-11-10 18:24:57 +0100

    First working version for stream implementation

--- ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-lmp/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -1,14 +1,23 @@
 package de.hpi.ddm.actors;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.CompletionStage;
 
+import akka.NotUsed;
 import akka.actor.AbstractLoggingActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSelection;
 import akka.actor.Props;
+import akka.stream.SourceRef;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import akka.stream.javadsl.StreamRefs;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
+import de.hpi.ddm.messages.SourceByteMessage;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -35,14 +44,6 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 		private T message;
 		private ActorRef receiver;
 	}
-
-	@Data @NoArgsConstructor @AllArgsConstructor
-	public static class BytesMessage<T> implements Serializable {
-		private static final long serialVersionUID = 4057807743872319842L;
-		private T bytes;
-		private ActorRef sender;
-		private ActorRef receiver;
-	}
 	
 	/////////////////
 	// Actor State //
@@ -60,7 +61,7 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	public Receive createReceive() {
 		return receiveBuilder()
 				.match(LargeMessage.class, this::handle)
-				.match(BytesMessage.class, this::handle)
+				.match(SourceByteMessage.class, this::handle)
 				.matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
 				.build();
 	}
@@ -68,42 +69,62 @@ public class LargeMessageProxy extends AbstractLoggingActor {
 	private void handle(LargeMessage<?> message) {
 		ActorRef receiver = message.getReceiver();
 		ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
-		File tempFile = null;
-		Kryo kryo = new Kryo();
-		try{
-			File tempDir = new File(System.getProperty("java.io.tmpdir"));
-			tempFile = File.createTempFile("largeMessage", ".tmp", tempDir);
-			FileOutputStream file = new FileOutputStream(tempFile);
-			Output output = new Output(file);
-			kryo.writeClassAndObject(output, message.getMessage());
-			output.close();
-			file.close();
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		//TODO Way to slow
+		byte[] serializedObject = toByte(message.getMessage());
+		Byte[] converted = new Byte[serializedObject.length];
+		for(int i = 0; i< serializedObject.length; i++)
+			converted[i] = Byte.valueOf(serializedObject[i]);
 
-		// This will definitely fail in a distributed setting if the serialized message is large!
-		// Solution options:
-		// 0.5. Serialize the object and store it on file and read.
-		// 1. Serialize the object and send its bytes batch-wise (make sure to use artery's side channel then).
-		// 2. Serialize the object and send its bytes via Akka streaming.
-		// 3. Send the object via Akka's http client-server component.
-		// 4. Other ideas ...
-		receiverProxy.tell(new BytesMessage<>(tempFile.getAbsolutePath(), this.sender(), message.getReceiver()), this.self());
+		Source<Byte, NotUsed> source = Source.from(Arrays.asList(converted));
+		SourceRef<Byte> sourceRef = source.runWith(StreamRefs.sourceRef(), this.context().system());
+
+		receiverProxy.tell(new SourceByteMessage(sourceRef, this.sender(), message.getReceiver()), this.self());
 	}
 
-	private void handle(BytesMessage<?> message) {
-		// Reassemble the message content, deserialize it and/or load the content from some local location before forwarding its content.
+	private byte[] toByte(Object object){
+		try {
+			Kryo kryo = new Kryo();
+			ByteArrayOutputStream stream = new ByteArrayOutputStream();
+			Output output = new Output(stream);
+			kryo.writeClassAndObject(output, object);
+			output.close();
+			stream.close();
+			return stream.toByteArray();
+		}catch(Exception e){
+			return null;
+		}
+	}
+
+	private void handle(SourceByteMessage message){
+		Sink sink = Sink.fold(new ArrayList<Byte>(), (aggr, next) -> add(aggr, (Byte)next));
+		Source source = message.getSourceRef().getSource();
+
+		final CompletionStage<ArrayList<Byte>> o = (CompletionStage<ArrayList<Byte>>) source.runWith(sink, this.context().system());
+		o.whenComplete((p,a ) -> handleTransmissionComplete(p,a, message.getReceiver(), message.getSender()));
+	}
+
+	private void handleTransmissionComplete(ArrayList<Byte> elements, Object exception, ActorRef receiver, ActorRef sender) {
 		Object object = null;
 		try{
-			String file = (String)message.getBytes();
-			FileInputStream inputStream = new FileInputStream(file);
+			Object[] array = elements.toArray();
+			byte[] converted = new byte[array.length];
+			for(int i = 0; i< array.length; i++)
+				converted[i] = (byte)array[i];
 			Kryo kryo = new Kryo();
-			Input input = new Input(inputStream);
+
+			ByteArrayInputStream stream = new ByteArrayInputStream(converted);
+			Input input = new Input(stream);
 			object = kryo.readClassAndObject(input);
 			input.close();
-		}catch(Exception e){}
-		message.getReceiver().tell(object, message.getSender());
+			receiver.tell(object, sender);
+		}catch(Exception e){
+			System.out.println(e);
+		}
+	}
+
+	private ArrayList<Byte> add (ArrayList<Byte> list, Byte next){
+		list.add(next);
+		return list;
 	}
 }
