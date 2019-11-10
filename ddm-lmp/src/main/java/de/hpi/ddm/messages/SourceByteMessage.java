c4ed56d8e84595f1c94df9e4bf55af18dcf781e6
Maximilian Schall <Maximilian.Schall@mail.de>
2019-11-10 18:24:57 +0100

    First working version for stream implementation

---
+++ ddm-lmp/src/main/java/de/hpi/ddm/messages/SourceByteMessage.java
@@ -0,0 +1,16 @@
+package de.hpi.ddm.messages;
+
+import akka.actor.ActorRef;
+import akka.stream.SourceRef;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SourceByteMessage{
+    private SourceRef<Byte> sourceRef;
+    private ActorRef sender;
+    private ActorRef receiver;
+}
