0ae209fc3fbab2f12e10b5a6ccf434dea37c4b04
Maximilian Schall <maxscha@mail.de>
2020-01-16 20:00:48 +0100

    Cache based Preaggregation

--- ddm-idd/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ ddm-idd/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -1,7 +1,6 @@
 package de.hpi.spark_tutorial
 
-import org.apache.spark.sql.{Dataset, Row, SparkSession}
-import org.apache.spark.sql.{Dataset, Encoder, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, Encoder, Row, SparkSession}
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.classification.DecisionTreeClassificationModel
 import org.apache.spark.ml.classification.DecisionTreeClassifier
@@ -9,6 +8,7 @@ import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
+import org.apache.spark.rdd.RDD
 
 object Sindy extends App {
 
@@ -55,48 +55,38 @@ object Sindy extends App {
     result
   }
 
-  def preaggregation(frame : Dataset[(String, String)], spark: SparkSession): Dataset[(String, Seq[String])] = {
+  def preaggregation(frame: Dataset[(String, String)], spark: SparkSession): Dataset[(String, Seq[String])] = {
     import spark.implicits._
 
     frame
       .groupByKey(p => p._1)
       .mapValues { case (key, values) => Set(values) }
       .reduceGroups((storage, value) => storage ++ value)
-      .map{case (key, value) => (key, value.seq.toList.distinct)}
+      .map { case (key, value) => (key, value.seq.toList.distinct) }
   }
 
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
     import spark.implicits._
 
     // Importing implicit encoders for standard library classes and tuples that are used as Dataset types
+
     val frames = inputs.map(path => spark.read
       .option("sep", ";")
-      .option("inferSchema", "false")
       .option("header", "true")
+      //      .option("inferSchema", "true")
       .csv(path)
     )
-
-    //    frames.flatMap(frame => frame.flatMap((test) => List(test)))
     val tuples_frames = frames.map(frame => {
       val columns = frame.columns
-      frame.flatMap(row => row.toSeq.map(p => String.valueOf(p)).zip(columns))
+      val cells = frame.flatMap(row => row.toSeq.map(p => String.valueOf(p)).zip(columns))
+      preaggregation(cells, spark)
     })
-    val frame = tuples_frames.head
-    frame.show()
-    val result = preaggregation(frame, spark)
 
-    result.show()
-
-    //    frame.groupByKey( t => t._1)
-    //    frames.map(frame => {
-    //    })
+    val cache_based_preaggregation = tuples_frames.reduce((acc, frame) => acc.union(frame))
+    System.out.println(cache_based_preaggregation.count())
+    cache_based_preaggregation.show()
 
 
-    //    results.show()
-
-    //    System.out.println(result.mkString);
-
-    //    frames.foreach(p => p.show())
     // TODO
   }
 }
