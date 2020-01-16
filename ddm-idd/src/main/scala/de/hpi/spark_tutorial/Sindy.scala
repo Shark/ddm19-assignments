f76b7c54beeb5a78562a38cfe5bbb5408d41c13b
Maximilian Schall <maxscha@mail.de>
2020-01-16 16:46:47 +0100

    Dataframe to tuples

--- ddm-idd/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ ddm-idd/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -1,11 +1,84 @@
 package de.hpi.spark_tutorial
 
 import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.{Dataset, Encoder, SparkSession}
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.DecisionTreeClassificationModel
+import org.apache.spark.ml.classification.DecisionTreeClassifier
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature.{IndexToString, StringIndexer, VectorIndexer}
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
 
-object Sindy {
+object Sindy extends App {
+
+  override def main(args: Array[String]): Unit = {
+
+    Logger.getLogger("org").setLevel(Level.OFF)
+    Logger.getLogger("akka").setLevel(Level.OFF)
+    //------------------------------------------------------------------------------------------------------------------
+    // Setting up a Spark Session
+    //------------------------------------------------------------------------------------------------------------------
+
+    // Create a SparkSession to work with Spark
+    val sparkBuilder = SparkSession
+      .builder()
+      .appName("SparkTutorial")
+      .master("local[4]") // local, with 4 worker cores
+    val spark = sparkBuilder.getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+    // Set the default number of shuffle partitions (default is 200, which is too high for local deployment)
+    spark.conf.set("spark.sql.shuffle.partitions", "8") //
+
+    // Importing implicit encoders for standard library classes and tuples that are used as Dataset types
+    import spark.implicits._
+
+    println("---------------------------------------------------------------------------------------------------------")
+    //------------------------------------------------------------------------------------------------------------------
+    // Inclusion Dependency Discovery (Homework)
+    //------------------------------------------------------------------------------------------------------------------
+
+    val inputs = List("region", "nation", "supplier", "customer", "part", "lineitem", "orders")
+      .map(name => s"data/TPCH/tpch_$name.csv")
+
+
+    time {
+      discoverINDs(inputs, spark)
+    }
+  }
+
+  def time[R](block: => R): R = {
+    val t0 = System.currentTimeMillis()
+    val result = block
+    val t1 = System.currentTimeMillis()
+    println(s"Execution: ${t1 - t0} ms")
+    result
+  }
 
   def discoverINDs(inputs: List[String], spark: SparkSession): Unit = {
+    import spark.implicits._
 
+    // Importing implicit encoders for standard library classes and tuples that are used as Dataset types
+    val frames = inputs.map(path => spark.read
+      .option("sep", ";")
+      .option("inferSchema", "false")
+      .option("header", "true")
+      .csv(path)
+    )
+
+    //    frames.flatMap(frame => frame.flatMap((test) => List(test)))
+    val frame = frames.head.toDF()
+    //    frame.groupByKey( t => t._1)
+    //    frames.map(frame => {
+    //    })
+    val columns = frame.columns
+    val results = frame.flatMap(row => row.toSeq.map(p => String.valueOf(p)).zip(columns))
+
+    results.show()
+
+    //    System.out.println(result.mkString);
+
+    //    frames.foreach(p => p.show())
     // TODO
   }
 }
